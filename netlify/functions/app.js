d7ee81aa48e3a93211894ceb78f10e0d6e954fdc
RansomBroker <yadisoke@gmail.com>
2024-11-12 19:42:19 +0700

    first commit

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -12,6 +12,7 @@ const indexRouter = require("../../routes/index");
 const app = express();
 
 // Setup view engine
+app.engine("pug", require("pug").__express);
 app.set("views", path.join(__dirname, "../../views"));
 app.set("view engine", "pug");
 
